afc248f442259e646c9bce684143d6ee2f64b558
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2024-06-03 02:04:22 +0200

    added example

---
+++ examples/show.rs
@@ -0,0 +1,19 @@
+use std::{collections::HashMap, ffi::OsString, path::Path};
+
+use dotenv::Result;
+
+fn main() -> Result<()> {
+    let mut env = HashMap::<OsString, OsString>::new();
+    dotenv::build()
+        .debug(true)
+        .strict(false)
+        .path(Path::new(env!("CARGO_MANIFEST_DIR")).join("examples").join("buggy-example.env"))
+        .config_env(&mut env)?;
+
+    for (key, value) in env {
+        let key = key.to_string_lossy();
+        println!("{key}={value:?}");
+    }
+
+    Ok(())
+}
